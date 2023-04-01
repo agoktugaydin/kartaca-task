a9f25aaace1e7563cba01584160a5c7fdf8a138e
agoktugaydin <goktugaydin36@gmail.com>
2023-04-01 20:25:17 +0300

    "Tebrikler, görev içeriğine ulaştın!"

--- register_deneme.py
+++ register_deneme.py
@@ -1,10 +1,18 @@
 import websockets
 import asyncio
 import json
+from decrypt import Decryptor
 
 # referans: https://stackoverflow.com/a/72629896
+
+
 async def write(websocket, message):
     await websocket.send(message)
+
+
+async def read_once(websocket):
+    message = await websocket.recv()
+    return message
 
 
 async def read(websocket):
@@ -12,12 +20,33 @@ async def read(websocket):
         message = await websocket.recv()
         print(message)
 
+
+def extract_registrationKey(text):
+    key = text.split("registrationKey : ")[1]
+    return key
+
+
 async def main():
+    decryptor = Decryptor()
+
     url = "wss://cekirdektenyetisenler.kartaca.com/ws"
+
     register_message = {"type": "REGISTER", "name": "Ahmed Goktug", "surname": "Aydin",
-                        "email": "agoktugaydin@gmail.com", "registrationKey": "85877b8cdaa78e56705287bb61bf8cc222b25715631b97108876ada3b061f102"}
-    json_register_message = json.dumps(register_message)
+                        "email": "agoktugaydin@gmail.com", "registrationKey": "xxxx"}
+
     async with websockets.connect(url) as ws:
+        first_response = await read_once(ws)
+
+        decrypted_first_response = decryptor.decrypt_message(first_response)
+        key_raw = extract_registrationKey(
+            decrypted_first_response)  # ex: 00000}"
+        key = key_raw[:-2]  # ex: 00000
+
+        print("KEY RAW -> ", key_raw, "KEY ->", key)
+        register_message["registrationKey"] = key
+        print("register_message ->", register_message)
+        json_register_message = json.dumps(register_message)
+
         await write(ws, json_register_message)
         await read(ws)
         await asyncio.Future()  # run forever
